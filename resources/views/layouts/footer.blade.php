e08a6a1a8c3106413bfc3f6fd77f4b9367ae8001
Adedaramola <paranok2017@gmail.com>
2021-09-11 01:24:55 +0100

    fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -38,8 +38,8 @@
     <div class="md:hidden">
         <ul class="mt-3" x-data="{ selected:null }">
             <li class="relative border-b border-gray-100">
-                <button x-on:click="selected !== 1 ? selected = 1 : selected = null" type="button" class="w-full px-4 py-2 flex
-            justify-between items-center text-sm bg-gray-50">
+                <button x-on:click="selected !== 1 ? selected = 1 : selected = null" type="button" class="w-full px-4 py-3 flex
+            justify-between items-center bg-gray-50">
                     <span>Help Information</span>
                     <span>
                         <x-ik-chevron-down class="w-7 h-7 text-black" />
@@ -47,7 +47,7 @@
                 </button>
                 <div class="relative overflow-hidden max-h-0 transition-all duration-150" style="" x-ref="container1"
                     x-bind:style="selected == 1 ? 'max-height: ' + $refs.container1.scrollHeight + 'px' : ''">
-                    <ul class="px-4 py-2 text-sm">
+                    <ul class="px-4 py-2">
                         <li><a href="#" class="inline-block mb-4 hover:text-indigo-400">Help</a></li>
                         <li><a href="#" class="inline-block mb-4 hover:text-indigo-400">Track Order</a></li>
                         <li><a href="#" class="inline-block hover:text-indigo-400">Delivery & returns</a></li>
@@ -55,8 +55,8 @@
                 </div>
             </li>
             <li class="relative border-b border-gray-100">
-                <button x-on:click="selected !== 2 ? selected = 2 : selected = null" type="button" class="w-full px-4 py-2 flex
-            justify-between items-center text-sm bg-gray-50">
+                <button x-on:click="selected !== 2 ? selected = 2 : selected = null" type="button" class="w-full px-4 py-3 flex
+            justify-between items-center bg-gray-50">
                     <span>About KUPPA</span>
                     <span>
                         <x-ik-chevron-down class="w-7 h-7 text-black" />
@@ -64,7 +64,7 @@
                 </button>
                 <div class="relative overflow-hidden max-h-0 transition-all duration-150" style="" x-ref="container2"
                     x-bind:style="selected == 2 ? 'max-height: ' + $refs.container2.scrollHeight + 'px' : ''">
-                    <ul class="px-4 py-2 text-sm">
+                    <ul class="px-4 py-2">
                         <li><a href="#" class="inline-block mb-4 hover:text-indigo-400">About us</a></li>
                         <li><a href="#" class="inline-block mb-4 hover:text-indigo-400">Careers at KUPPA</a></li>
                         <li><a href="#" class="inline-block hover:text-indigo-400">Corporate responsibility</a></li>
@@ -72,8 +72,8 @@
                 </div>
             </li>
             <li class="relative border-b border-gray-100">
-                <button x-on:click="selected !== 3 ? selected = 3 : selected = null" type="button" class="w-full px-4 py-2 flex
-            justify-between items-center text-sm bg-gray-50">
+                <button x-on:click="selected !== 3 ? selected = 3 : selected = null" type="button" class="w-full px-4 py-3 flex
+            justify-between items-center bg-gray-50">
                     <span>Contact and support</span>
                     <span>
                         <x-ik-chevron-down class="w-7 h-7 text-black" />
@@ -81,7 +81,7 @@
                 </button>
                 <div class="relative overflow-hidden max-h-0 transition-all duration-150" style="" x-ref="container3"
                     x-bind:style="selected == 3 ? 'max-height: ' + $refs.container3.scrollHeight + 'px' : ''">
-                    <ul class="px-4 py-2 text-sm">
+                    <ul class="px-4 py-2">
                         <li><a href="#" class="inline-block mb-4 hover:text-indigo-400">About us</a></li>
                         <li><a href="#" class="inline-block mb-4  hover:text-indigo-400">Careers at KUPPA</a></li>
                         <li><a href="#" class="inline-block  hover:text-indigo-400">Corporate responsibility</a>
